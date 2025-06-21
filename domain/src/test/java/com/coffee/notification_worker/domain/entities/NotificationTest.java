58e05afc814cdac683589563ed2a07b8c7b04775
Nikita <ns_stepanov@inbox.ru>
2025-06-21 05:02:38 +0300

    Init tests in domain

---
+++ domain/src/test/java/com/coffee/notification_worker/domain/entities/NotificationTest.java
@@ -0,0 +1,24 @@
+package com.coffee.notification_worker.domain.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotificationTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void getContent() {
+    }
+}
